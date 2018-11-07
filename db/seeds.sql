22c72d6068d27c73637428cb9741882765856567
popcorn <40187116+lylekarim@users.noreply.github.com>
2018-11-07 14:01:03 -0500

    commit mvp

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meeses Delux', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bobbi Jr.', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester with Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson Portobello', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden Triple', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Owens Minor League', false);
