1608f1e0187120554b658e607d43a27b7fb691b3
popcorn <40187116+lylekarim@users.noreply.github.com>
2018-11-07 19:11:10 -0500

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,16 @@ CREATE TABLE burgers
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+
+USE wycd68cg1hcsuj;
+
+CREATE TABLE burgers (
+ id INT auto_increment not null,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+ createdAt timestamp not null,
+ PRIMARY KEY(id)
+
+
+);
